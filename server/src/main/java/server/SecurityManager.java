9f4a9912d090f49ec3978f0c333e2aa1fa4c499c
Nightlyside <nightlyside@gmail.com>
2021-02-26 10:08:44 +0100

    Adding security manager

---
+++ server/src/main/java/server/SecurityManager.java
@@ -0,0 +1,61 @@
+package server;
+
+import database.entities.User;
+
+import java.security.Permission;
+
+public class SecurityManager {
+
+    private boolean loggedIn = false;
+    private User loggedUser;
+
+    /** private constructor */
+    private SecurityManager() {}
+
+    /** unique instance */
+    private static SecurityManager INSTANCE = null;
+
+    /** unique instantiation and getting the instance
+     * this function is threadsafe */
+    public static synchronized SecurityManager getInstance()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = new SecurityManager();
+        }
+        return INSTANCE;
+    }
+
+    /** anti-deserialization security */
+    private Object readResolve() {
+        return INSTANCE;
+    }
+
+    public boolean isUserLoggedIn() {
+        return this.loggedIn;
+    }
+
+    public User getLoggedUser() {
+        // security if the user is note logged in
+        if (!this.isUserLoggedIn()) { return null; }
+
+        return this.loggedUser;
+    }
+
+    public void setLoggedUser(User newUser) {
+        this.loggedIn = true;
+        this.loggedUser = newUser;
+    }
+
+    public void removeLoggedUser() {
+        this.loggedIn = false;
+        this.loggedUser = null;
+    }
+
+    public void hasRoleHighEnoughTo(User.Role requiredRole) throws UnauthorizedException {
+        // comparing roles to check if the user can do it
+        if (this.loggedUser.getRole().compareTo(requiredRole) < 0) {
+            throw new UnauthorizedException("Role not high enough to perform action");
+        }
+    }
+}
