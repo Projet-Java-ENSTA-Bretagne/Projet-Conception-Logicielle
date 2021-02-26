dd72e060641e3b6a4418374e950d69009edf6d3c
Sytrics <guillaume.leinen@ensta-bretagne.org>
2021-02-26 10:49:43 +0100

    config of maven-fxml wrong

--- client/src/main/java/MainClient.java
+++ client/src/main/java/MainClient.java
@@ -1,18 +1,39 @@
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class MainClient {
+public class MainClient extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("pages/login.fxml"));
+            primaryStage.setTitle("Login");
+            primaryStage.setScene(new Scene(root, 1024, 600));
+            primaryStage.show();
+        }
+        catch (Exception e) {
+            System.out.println("erreur de chargement de la page" + e );
+        }
+
+    }
 
     public static void main(String[] args) {
         // Setting logging level
         Configurator.setRootLevel(Level.DEBUG);
-
         TCPClient tcpClient = new TCPClient("localhost", 6666);
-
+        launch(args);
         if (tcpClient.connectToServer()) {
             String request = RequestBuilder.buildWithoutData("PING").toString();
             tcpClient.sendRequest(request);
             tcpClient.disconnectFromServer();
         }
+
     }
+
 }
+
+
