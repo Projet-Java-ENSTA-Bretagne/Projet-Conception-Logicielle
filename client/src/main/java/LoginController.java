2df926f471567f6627a4f693c79a1d4f63ede53a
Ays-s <73881095+Ays-s@users.noreply.github.com>
2021-02-27 14:44:26 +0100

    add LoginController
    just test login with println
    with mask/unmask button

---
+++ client/src/main/java/LoginController.java
@@ -0,0 +1,46 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private JFXTextField usernameField;
+
+    @FXML
+    private JFXPasswordField passwordField;
+
+    @FXML
+    private JFXButton login;
+
+    @FXML
+    private JFXButton showPasswordButton;
+    private String password;
+
+    @FXML
+    void makeLogin() {
+        if(usernameField.getText().equals("test")&&passwordField.getText().equals("test"))
+        {
+            System.out.println("Welcome");
+        }
+    }
+
+    @FXML
+    void unmaskPassword() {
+        password = passwordField.getText();
+        passwordField.clear();
+        passwordField.setPromptText(password);
+    }
+
+    @FXML
+    void maskPassword() {
+        passwordField.setText(password);
+        passwordField.setPromptText("Password");
+    }
+
+}
