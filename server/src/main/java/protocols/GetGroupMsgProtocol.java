0c9582d33c678ff60aa931d9234e5e0e5b6170a1
cookiehacker <erwanaubry45@protonmail.com>
2021-03-09 08:54:48 +0100

    Development of GetGroupMsg protocol

--- server/src/main/java/protocols/GetGroupMsgProtocol.java
+++ server/src/main/java/protocols/GetGroupMsgProtocol.java
@@ -1,12 +1,20 @@
 package protocols;
 
+import com.j256.ormlite.dao.Dao;
+import database.entities.Group;
+import database.entities.Message;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import server.DatabaseContext;
 import server.IContext;
+import server.ResponseBuilder;
 
 import java.io.BufferedReader;
 import java.io.PrintStream;
+import java.sql.SQLException;
+import java.util.List;
 
 public class GetGroupMsgProtocol implements IProtocol {
 
@@ -14,7 +22,72 @@ public class GetGroupMsgProtocol implements IProtocol {
 
     public static String requestName = "getGroupMsg";
 
-    public void execute(IContext ctx, BufferedReader inStream, PrintStream outStream, JSONObject request) {
+    /**
+     * Function allow to execute the command createUser send by a client.
+     *
+     * Example of request send by the client :
+     *
+     * {
+     *     "command" : "getGroupMsg",
+     *     "args" : {
+     *         "group_id" : "c9d36c97-4e3d-4b0c-9a78-50f76e7f5589",
+     *         "index" : 0,
+     *         "limit" : 5
+     *     }
+     * }
+     *
+     * @param ctx : The context
+     * @param inStream : The input stream of the client
+     * @param outStream : The output stream of the server
+     * @param request : The request of the client
+     * @throws SQLException
+     */
+    public void execute(IContext ctx, BufferedReader inStream, PrintStream outStream, JSONObject request) throws SQLException {
+        JSONObject data = request.getJSONObject("args");
+        String group_id  = data.getString("group_id");
+        int index = data.getInt("index");
+        int limit = data.getInt("limit");
 
+        Dao<Group,String> groupDao = ((DatabaseContext) ctx).getDatabaseManager().getGroupDao();
+
+        List<Group> matchingGroups = groupDao.queryBuilder().where().in("id", group_id).query();
+
+        if(matchingGroups.size() == 0)
+        {
+            ResponseBuilder.sendDeniedError(outStream, request, "There is no group with the ID: " + group_id);
+            return;
+        }
+
+        Dao<Message,String> messageDao = ((DatabaseContext) ctx).getDatabaseManager().getMessageDao();
+
+        List<Message> matchingMessages = messageDao.queryBuilder().where().in("groupID", group_id).query();
+
+        int sizeneed = ((index+limit)+1);
+
+        List<Message> showMessage;
+
+        if(matchingMessages.size() < sizeneed) {
+
+            showMessage = matchingMessages.subList(index, sizeneed - (sizeneed-matchingMessages.size()));
+        }
+        else
+        {
+            showMessage = matchingMessages.subList(index, index+limit);
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        for(Message m : showMessage)
+        {
+            array.put(m.toJSON());
+        }
+
+        jsonObject.put("msg",array);
+
+        System.out.println(jsonObject.toString(2));
+
+        outStream.println(ResponseBuilder.buildData(
+                request, ResponseBuilder.StatusCode.OK, jsonObject));
     }
 }
