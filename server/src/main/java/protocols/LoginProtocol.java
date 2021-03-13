b5c8b50e5518ab85d96c60c0e3044e27c79b5da7
cookiehacker <erwanaubry45@protonmail.com>
2021-03-13 15:03:20 +0100

    login with hashed_password in SHA-512

--- server/src/main/java/protocols/LoginProtocol.java
+++ server/src/main/java/protocols/LoginProtocol.java
@@ -5,6 +5,7 @@ import database.SecurityManager;
 import database.entities.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bouncycastle.util.encoders.Hex;
 import org.json.JSONObject;
 import server.DatabaseContext;
 import server.IContext;
@@ -12,6 +13,9 @@ import server.ResponseBuilder;
 
 import java.io.BufferedReader;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -41,19 +45,30 @@ public class LoginProtocol implements IProtocol {
         Dao<User, String> userDao = ((DatabaseContext) ctx).getDatabaseManager().getUserDao();
 
         // check if user exist
-        List<User> matchingUsers = userDao.queryBuilder().where().
-                eq("name", username).and().eq("password", password).
-                query();
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-512");
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            String sha512pass = new String(Hex.encode(hash));
 
-        // if there is already a user
-        if (matchingUsers.size() == 0) {
-            ResponseBuilder.forRequest(request, outStream).deniedError("Wrong username/password combinaison");
-            return;
+            List<User> matchingUsers = userDao.queryBuilder().where().
+                    eq("name", username).and().eq("password", sha512pass).
+                    query();
+
+            // if there is already a user
+            if (matchingUsers.size() == 0) {
+                ResponseBuilder.forRequest(request, outStream).deniedError("Wrong username/password combinaison");
+                return;
+            }
+
+            User matchedUser = matchingUsers.get(0);
+            SecurityManager.getInstance().setLoggedUser(matchedUser);
+            // then tell the user everything went ok and sending the data
+            ResponseBuilder.forRequest(request, outStream).ok("Successfully logged in.");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            ResponseBuilder.forRequest(request, outStream).serverError("Error when encoding password for verification");
         }
 
-        User matchedUser = matchingUsers.get(0);
-        SecurityManager.getInstance().setLoggedUser(matchedUser);
-        // then tell the user everything went ok and sending the data
-        ResponseBuilder.forRequest(request, outStream).ok("Successfully logged in.");
     }
 }
