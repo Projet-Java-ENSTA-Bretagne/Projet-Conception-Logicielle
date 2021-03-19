365692c442da301806c770089da318b20ec50b14
Nightlyside <nightlyside@gmail.com>
2021-03-19 08:26:58 +0100

    Creating helper to test protocols

--- server/src/test/java/protocols/PingProtocolTest.java
+++ server/src/test/java/protocols/PingProtocolTest.java
@@ -1,6 +1,7 @@
 package protocols;
 
 import database.DatabaseManager;
+import database.UserNotLoggedException;
 import junit.framework.TestCase;
 import org.json.JSONObject;
 import server.DatabaseContext;
@@ -10,31 +11,19 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
+import java.sql.SQLException;
 
 public class PingProtocolTest extends TestCase {
 
-    public void testPing() {
-        // 1. Creating new instance of protocol
-        PingProtocol instance = new PingProtocol();
-
-        // 2. Creating context
-        DatabaseManager dm = new DatabaseManager("database.db");
-        IContext ctx = new DatabaseContext(dm);
-
-        // 3. Creating input and output streams
-        BufferedReader inStream = new BufferedReader(new StringReader(""));
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        PrintStream outStream = new PrintStream(os);
-
-        // 4. Creating the request to the protocol
+    public void testPing() throws SQLException, UserNotLoggedException {
+        // Forging the request
         JSONObject request = new JSONObject();
         request.put("command", "PING");
 
-        // 5. Executing the protocol and getting back the response
-        instance.execute(ctx, inStream, outStream, request);
-        JSONObject response = new JSONObject(os.toString());
+        // Getting the response from the protocol
+        JSONObject response = ProtocolTestingHelper.executeProtocol(new PingProtocol(), request);
 
-        // 6. Checking the response data
+        // Checking the response data
         assertEquals(response.getString("status"), "OK");
         assertEquals(response.getJSONObject("data").getString("message"), "PONG");
 
---
+++ server/src/test/java/protocols/ProtocolTestingHelper.java
@@ -0,0 +1,39 @@
+package protocols;
+
+import database.DatabaseManager;
+import database.UserNotLoggedException;
+import org.json.JSONObject;
+import server.DatabaseContext;
+import server.IContext;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.StringReader;
+import java.sql.SQLException;
+
+public class ProtocolTestingHelper {
+
+    public PrintStream outStream;
+    public ByteArrayOutputStream os;
+    public BufferedReader inStream;
+    public JSONObject response;
+    public IContext context;
+
+    public ProtocolTestingHelper() {
+        // 1. Creating context
+        DatabaseManager dm = new DatabaseManager("database.db");
+        context = new DatabaseContext(dm);
+
+        // 2. Creating input and output streams
+        inStream = new BufferedReader(new StringReader(""));
+        os = new ByteArrayOutputStream();
+        outStream = new PrintStream(os);
+    }
+
+    public static JSONObject executeProtocol(IProtocol protocol, JSONObject request) throws SQLException, UserNotLoggedException {
+        ProtocolTestingHelper instance = new ProtocolTestingHelper();
+        protocol.execute(instance.context, instance.inStream, instance.outStream, request);
+        return new JSONObject(instance.os.toString());
+    }
+}
