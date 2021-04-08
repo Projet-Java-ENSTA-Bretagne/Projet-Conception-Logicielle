37b808f098e688fe0ffcc2bd6950a4082eb93a9b
sytrics <guillaume.leinen@ensta-bretagne.org>
2021-04-08 10:43:55 +0200

    requestbuilder test

---
+++ client/src/test/java/RequestBuilderTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+import networking.RequestBuilder;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestBuilderTest {
+
+    @Test
+    public void WithDataTest() {
+        JSONObject actual = RequestBuilder.buildWithoutData("test");
+
+        assertTrue(actual.get("command") != null);
+        assertEquals(actual.getString("command"), "test");
+    }
+    @Test
+    public void WithoutDataTest() {
+        JSONObject data = new JSONObject();
+        data.put("data", "testdata");
+
+        JSONObject actual = RequestBuilder.buildWithData("test", data);
+        assertTrue(actual.get("command") != null);
+        assertEquals(actual.getString("command"), "test");
+        assertEquals(actual.getJSONObject("args"), data);
+
+    }
+}
